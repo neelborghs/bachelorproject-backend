6929c676f269d1339d475d1edefa2fa020b25534
Neel <neelborghs@gmail.com>
2017-05-20 21:50:14 +0700

    Neel: test add module - url nog veranderen naar juiste gateway

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -31,7 +31,7 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 						html =   "<div class='w3-card-4' style='width:100%'><div class='w3-container'><br><p>No modules registered!</p><hr><img src='/images/plant.png' alt='Avatar' class='w3-left w3-circle w3-margin-right' style='width: 10em'><p>You don't have any modules registered on this account.<p/><p>Go to the Android application to add some plant modules to this account or login with another account.</p><p>If you have any trouble connecting your module, you can always read the instructions</p></div><div class='w3-container'><br></div></div>";
 		request({url: 'https://api.myjson.com/bins/121485', json: true}, function(err, res, json) {
 			if (err) {
-				
+
 			}
 			//console.log(json);
 			addModule = "";
@@ -41,38 +41,41 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 
 	}
 		//AUTOMATIC MODULE ADD --- END ---
-	for (i in jsonObject){
-		if (jsonObject[i].moisture < 50)
-		{
-			html += "<div class='w3-card-4red' style='width:100%'>";
-		}
-		if (jsonObject[i].moisture >= 50 && jsonObject[i].moisture < 55){
-						html += "<div class='w3-card-4orange' style='width:100%'>";
-		}
-		if (jsonObject[i].moisture >= 55){
-						html += "<div class='w3-card-4green' style='width:100%'>";
-		}
-		html += "<div class='w3-container'><br><p>Module ID: " + jsonObject[i].module_id + "</p><hr><table><tr>";
-		if (jsonObject[i].light==1)
-		{
-			html +=
-									"<td><img src='/images/light.png' width='70%'><br><br><p>Light</p></td>";
-		}
 		else{
-			html +=
-									"<td><img src='/images/dark.png' width='70%'><br><br><p>Dark</p></td>";
+			for (i in jsonObject){
+				if (jsonObject[i].moisture < 50)
+				{
+					html += "<div class='w3-card-4red' style='width:100%'>";
+				}
+				if (jsonObject[i].moisture >= 50 && jsonObject[i].moisture < 55){
+								html += "<div class='w3-card-4orange' style='width:100%'>";
+				}
+				if (jsonObject[i].moisture >= 55){
+								html += "<div class='w3-card-4green' style='width:100%'>";
+				}
+				html += "<div class='w3-container'><br><p>Module ID: " + jsonObject[i].module_id + "</p><hr><table><tr>";
+				if (jsonObject[i].light==1)
+				{
+					html +=
+											"<td><img src='/images/light.png' width='70%'><br><br><p>Light</p></td>";
+				}
+				else{
+					html +=
+											"<td><img src='/images/dark.png' width='70%'><br><br><p>Dark</p></td>";
+				}
+				html += 								"<td><img src='/images/hum.png' width='70%'><br><br><p>" +jsonObject[i].humidity + "%</p></td>" +
+												"<td><img src='/images/temp.png' width='70%'><br><br><p>" +jsonObject[i].temperature + "°C</p></td>" +
+												"<td><img src='/images/soil.png' width='70%'><br><br><p>" +jsonObject[i].moisture + "%</p></td>" +
+											"</tr></table></div></div><br><br>";
+				if (i==numberObjects -1){
+					html+= "<br><br>"
+				}
+			}
+			if (req.user.profile_picture_url!=null){
+				profilePicture += "<li role='presentation'><img src='" + req.user.profile_picture_url + "' class='w3-left w3-round-xxlarge w3-margin-right' style='height:3em'></li>"
+			}
 		}
-		html += 								"<td><img src='/images/hum.png' width='70%'><br><br><p>" +jsonObject[i].humidity + "%</p></td>" +
-										"<td><img src='/images/temp.png' width='70%'><br><br><p>" +jsonObject[i].temperature + "°C</p></td>" +
-										"<td><img src='/images/soil.png' width='70%'><br><br><p>" +jsonObject[i].moisture + "%</p></td>" +
-									"</tr></table></div></div><br><br>";
-		if (i==numberObjects -1){
-			html+= "<br><br>"
-		}
-	}
-	if (req.user.profile_picture_url!=null){
-		profilePicture += "<li role='presentation'><img src='" + req.user.profile_picture_url + "' class='w3-left w3-round-xxlarge w3-margin-right' style='height:3em'></li>"
-	}
+
 
 		res.render('index', {html: html, name: name, profilePicture: profilePicture});
 
