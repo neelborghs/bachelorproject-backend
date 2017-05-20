3d1ed9129f5cf0d399d190f1fd89ef188ef3f656
Neel <neelborghs@gmail.com>
2017-05-20 22:13:10 +0700

    Neel: test add module - url nog veranderen naar juiste gateway

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -19,20 +19,19 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 		//AUTOMATIC MODULE ADD --- START ---
 	if (req.user.user_id == null){
 						html =   "<div class='w3-card-4' style='width:100%'><div class='w3-container'><br><p>No modules registered!</p><hr><img src='/images/plant.png' alt='Avatar' class='w3-left w3-circle w3-margin-right' style='width: 10em'><p>You don't have any modules registered on this account.<p/><p>Go to the Android application to add some plant modules to this account or login with another account.</p><p>If you have any trouble connecting your module, you can always read the instructions</p></div><div class='w3-container'><br></div></div>";
-						sleep(500);
 		request({url: 'https://api.myjson.com/bins/s7ogl', json: true}, function(err, res, json) {
 			if (err) {
 
 			}
 			//console.log(json);
 			addModule = "";
-			addModule = json[1].string;
-			html =   "<div class='w3-card-4' style='width:100%'>Naar link geweest</div>";
+			addModule = json.string;
+			html =   "<div class='w3-card-4' style='width:100%'>Naar link geweest "+ addModule +"</div>";
 		});
 		sleep(5000);
 	}
 		//AUTOMATIC MODULE ADD --- END ---
-		if (req.user.user_id != null){
+		else{
 
 			request({url: 'https://ezgreen.herokuapp.com/api/modules/user/' + req.user.user_id, json: true}, function(err, res, json) {
 				if (err) {
