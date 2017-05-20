cbe00ca4b9d45611c867389741f404e56775e9c8
Neel <neelborghs@gmail.com>
2017-05-20 23:52:02 +0700

    Neel: test add module - url nog veranderen naar juiste gateway

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -25,7 +25,8 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 		request({url: 'https://api.myjson.com/bins/s7ogqsdfqsdfsqdfl', json: true}, function(err, res, json) {
 			if(err){
 				html =   "<div class='w3-card-4' style='width:100%'><div class='w3-container'><br><p>No modules found!</p><hr><img src='/images/plant.png' alt='Avatar' class='w3-left w3-circle w3-margin-right' style='width: 10em'><p>There were no modules found.<p/><p>Make sure to first plug in the modules and then the gateway.</p><p>Once you did that you can refresh this page to automatically link it to your account.</p></div><div class='w3-container'><br></div></div>";
-				res.render('index', {html: html});
+				res.redirect('/users/login');
+				//res.render('index', {html: html});
 				throw err;
 			}
 			//console.log(json);
