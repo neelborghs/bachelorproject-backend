7d5c3cd27f315167db66c58fb4ac34bb63818139
Neel <neelborghs@gmail.com>
2017-05-20 22:29:55 +0700

    Neel: test add module - url nog veranderen naar juiste gateway

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -32,7 +32,9 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 
 		});
 		sleep(500);
-		res.render('index', {html: html, name: name, profilePicture: profilePicture});
+		if (addModule==""){
+			res.redirect('/dashboard');
+		}
 	}
 		//AUTOMATIC MODULE ADD --- END ---
 		else{
@@ -81,11 +83,11 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 			if (req.user.profile_picture_url!=null){
 				profilePicture += "<li role='presentation'><img src='" + req.user.profile_picture_url + "' class='w3-left w3-round-xxlarge w3-margin-right' style='height:3em'></li>"
 			}
-			res.render('index', {html: html, name: name, profilePicture: profilePicture});
+
 		}
 
 
-
+		res.render('index', {html: html, name: name, profilePicture: profilePicture});
 
 
 });
