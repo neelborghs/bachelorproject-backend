544fc82f726d2a037d7b5fe0f04c268af9b7b0d7
Neel <neelborghs@gmail.com>
2017-05-20 17:44:30 +0700

    Neel: test afbeelding header

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -25,7 +25,8 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 	sleep(500); // sleep for 0.5 second, this helps refreshing the data
 	var numberObjects = Object.keys(jsonObject).length;
 	//html += "<h1>number objects: " + numberObjects + "</h1>"; //For testing numberObjects
-	if (numberObjects == 0){
+	//if (numberObjects == 0){
+	if (req.user.user_id == null){
 		html =   "<div class='w3-card-4' style='width:100%'><div class='w3-container'><br><p>No modules registered!</p><hr><img src='/images/plant.png' alt='Avatar' class='w3-left w3-circle w3-margin-right' style='width: 10em'><p>You don't have any modules registered on this account.<p/><p>Go to the Android application to add some plant modules to this account or login with another account.</p><p>If you have any trouble connecting your module, you can always read the instructions</p></div><div class='w3-container'><br></div></div>";
 	}
 	for (i in jsonObject){
