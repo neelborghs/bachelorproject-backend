b70276cc371cbbf24bb90ef2d7e4a4a399ed811e
Neel <neelborghs@gmail.com>
2017-05-20 23:13:49 +0700

    Neel: test add module - url nog veranderen naar juiste gateway

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -18,26 +18,80 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 	//if (numberObjects == 0){
 		//AUTOMATIC MODULE ADD --- START ---
 	if (req.user.user_id == null){
+		var newUserId = null;
 						html =   "<div class='w3-card-4' style='width:100%'><div class='w3-container'><br><p>No modules registered!</p><hr><img src='/images/plant.png' alt='Avatar' class='w3-left w3-circle w3-margin-right' style='width: 10em'><p>You don't have any modules registered on this account.<p/><p>Go to the Android application to add some plant modules to this account or login with another account.</p><p>If you have any trouble connecting your module, you can always read the instructions</p></div><div class='w3-container'><br></div></div>";
 		request({url: 'https://api.myjson.com/bins/s7ogl', json: true}, function(err, res, json) {
 			if (err) {
-
+				throw err;
 			}
 			//console.log(json);
 			addModule = null;
 			addModule = json;
 			for (i in addModule){
+				newUserId = addModule[i].string;
 				html =   "<div class='w3-card-4' style='width:100%'>Naar link geweest "+ addModule[i].string +"</div>";
 			}
-
+			profile.getUserByEmail(req.user.email, function(err, user){
+				if (err) {
+					throw err;
+				}
+				else{
+					user.user_id = newUserId;
+				}
+			}
 		});
 		sleep(500);
-
+		if (addModule!=null){
+			request({url: 'https://ezgreen.herokuapp.com/api/modules/user/' + id, json: true}, function(err, res, json) {
+				if (err) {
+					throw err;
+				}
+				//console.log(json);
+				jsonObject = "";
+				jsonObject = json;
+				console.log(jsonObject);
+			});
+			//Set dynamic data here
+			sleep(500); // sleep for 0.5 second, this helps refreshing the data
+			html = "";
+			for (i in jsonObject){
+				if (jsonObject[i].moisture < 50)
+				{
+					html += "<div class='w3-card-4red' style='width:100%'>";
+				}
+				if (jsonObject[i].moisture >= 50 && jsonObject[i].moisture < 55){
+								html += "<div class='w3-card-4orange' style='width:100%'>";
+				}
+				if (jsonObject[i].moisture >= 55){
+								html += "<div class='w3-card-4green' style='width:100%'>";
+				}
+				html += "<div class='w3-container'><br><p>Module ID: " + jsonObject[i].module_id + "</p><hr><table><tr>";
+				if (jsonObject[i].light==1)
+				{
+					html +=
+											"<td><img src='/images/light.png' width='70%'><br><br><p>Light</p></td>";
+				}
+				else{
+					html +=
+											"<td><img src='/images/dark.png' width='70%'><br><br><p>Dark</p></td>";
+				}
+				html += 								"<td><img src='/images/hum.png' width='70%'><br><br><p>" +jsonObject[i].humidity + "%</p></td>" +
+												"<td><img src='/images/temp.png' width='70%'><br><br><p>" +jsonObject[i].temperature + "°C</p></td>" +
+												"<td><img src='/images/soil.png' width='70%'><br><br><p>" +jsonObject[i].moisture + "%</p></td>" +
+											"</tr></table></div></div><br><br>";
+				if (i==numberObjects -1){
+					html+= "<br><br>"
+				}
+			}
+			if (req.user.profile_picture_url!=null){
+				profilePicture += "<li role='presentation'><img src='" + req.user.profile_picture_url + "' class='w3-left w3-round-xxlarge w3-margin-right' style='height:3em'></li>"
+			}
+		}
 	}
 		//AUTOMATIC MODULE ADD --- END ---
 		else{
 
-			request({url: 'https://ezgreen.herokuapp.com/api/modules/user/' + req.user.user_id, json: true}, function(err, res, json) {
+			request({url: 'https://ezgreen.herokuapp.com/api/modules/user/' + id, json: true}, function(err, res, json) {
 				if (err) {
 					throw err;
 				}
@@ -86,9 +140,7 @@ router.get('/dashboard', ensureAuthenticated, function(req, res){
 
 
 		res.render('index', {html: html, name: name, profilePicture: profilePicture});
-		if (addModule!=null){
-			res.redirect(req.get('index'));
-		}
+
 
 });
 
