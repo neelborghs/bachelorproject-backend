33c4ccc7177957587142461c3c1d8ea5e43af2f8
Neel <neelborghs@gmail.com>
2017-05-22 16:12:08 +0700

    Test bad link

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,17 +8,18 @@ var addModule = null;
 const profile = require('../functions/profile');
 const html2 = "<div class='w3-card-4' style='width:100%'><div class='w3-container'><br><p>No modules found!</p><hr><img src='/images/plant.png' alt='Avatar' class='w3-left w3-circle w3-margin-right' style='width: 10em'><p>There were no modules found.<p/><p>Make sure to first plug in the modules and then the gateway.</p><p>Once you did that you can refresh this page to automatically link it to your account.</p></div><div class='w3-container'><br></div></div>";
 var html = "";
+var name = "";
+var email = "";
 // Get Homepage
 router.get('/dashboard', ensureAuthenticated, function(req, res){
-	var name = req.user.first_name;
-	var email = req.user.email;
+	name = req.user.first_name;
+	email = req.user.email;
 	var profilePicture = ""
 	var successUserId = "";
 	var numberObjects = Object.keys(jsonObject).length;
 	//html += "<h1>number objects: " + numberObjects + "</h1>"; //For testing numberObjects
 	//if (numberObjects == 0){
 		//AUTOMATIC MODULE ADD --- START ---
-		sleep(500);
 	if (req.user.user_id==null){
 		request({url: 'https://api.myjqsdfqsdfson.com/bins/yzwz5', json: true}, function(err, res, json) {
 			if(err)
